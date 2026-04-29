4c5d742c44e5ce35d2ba4d0a2c16345c4162aefa
ricardobdenadai01 <ricardobdenadai01@gmail.com>
2026-04-29 12:36:20 -0300

    Update image assets in Vantagens component, removing papel5.png and papel6.png, and replace Swiper with a grid layout for better performance.

--- src/components/Vantagens.tsx
+++ src/components/Vantagens.tsx
@@ -1,15 +1,8 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/pagination";
-
 const VANTAGENS = [
   "/images/papel1.png",
   "/images/papel2.png",
   "/images/papel3.png",
   "/images/papel4.png",
-  "/images/papel5.png",
-  "/images/papel6.png",
 ];
 
 export default function Vantagens() {
@@ -17,25 +10,10 @@ export default function Vantagens() {
     <section className="py-8 px-6 md:px-24">
       <img src="/banners/Banner.png" alt="Banner" className="mx-auto w-full" />
 
-      <div className="mt-8">
-        <Swiper
-          modules={[Autoplay, Pagination]}
-          slidesPerView={1}
-          spaceBetween={16}
-          loop={true}
-          autoplay={{ delay: 3000, disableOnInteraction: false }}
-          breakpoints={{
-            768: { slidesPerView: 2, spaceBetween: 16 },
-            1024: { slidesPerView: 4, spaceBetween: 16 },
-          }}
-          className="pb-10"
-        >
-          {VANTAGENS.map((src) => (
-            <SwiperSlide key={src}>
-              <img src={src} alt="Vantagem" className="w-8/10 mx-auto" />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+      <div className="mt-8 grid grid-cols-2 gap-4 lg:grid-cols-4">
+        {VANTAGENS.map((src) => (
+          <img key={src} src={src} alt="Vantagem" className="w-full mx-auto" />
+        ))}
       </div>
     </section>
   );
